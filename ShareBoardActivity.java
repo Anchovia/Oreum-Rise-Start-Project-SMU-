228f6dd5f051028f2714635b4a408de18c0c9d37
Carolsuki <83635402+Carolsuki@users.noreply.github.com>
2021-05-14 13:35:16 +0900

    Create ShareBoardActivity.java

---
+++ ShareBoardActivity.java
@@ -0,0 +1,56 @@
+package com.example.smuproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ShareBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_board);
+
+        // 상단바 코드 부분
+        {
+            // 셋팅 아이콘 선언
+            ImageButton button_gotoSetting = findViewById(R.id.button_gotoSetting);
+            button_gotoSetting.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 셋팅 아이콘을 클릭했을때 SettingActivity로 이동
+                    Intent intent = new Intent(ShareBoardActivity.this, SettingActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            // 내정보 아이콘 선언
+            ImageButton button_gotoMyInfo = findViewById(R.id.button_gotoMyInfo);
+            button_gotoMyInfo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 내정보 아이콘을 클릭했을때 MyInfoActivity로 이동
+                    Intent intent = new Intent(ShareBoardActivity.this, MyInfoActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        // 하단바 코드 부분
+        {
+            // 홈 버튼 선언
+            ImageButton Button_gotoHome = findViewById(R.id.Button_gotoHome);
+            Button_gotoHome.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 홈 버튼을 클릭했을때 MainActivity으로 이동
+                    Intent intent = new Intent(ShareBoardActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+}
