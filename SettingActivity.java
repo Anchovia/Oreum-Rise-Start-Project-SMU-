5775b752ee54d97a70c1662ab508142e4b1e0f2d
Carolsuki <83635402+Carolsuki@users.noreply.github.com>
2021-05-14 13:33:59 +0900

    Create SettingActivity.java

---
+++ SettingActivity.java
@@ -0,0 +1,35 @@
+package com.example.smuproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SettingActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        Button button_logout = findViewById(R.id.button_logout);
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 로그아웃 하기
+                mFirebaseAuth.signOut();
+
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
